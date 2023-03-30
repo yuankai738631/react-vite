cf3d9b712c975881e19d5a8b15f34e31e448bf64
DESKTOP-31KA96E\Admin <kaiyuanpupu@outlook.com>
2023-03-31 02:22:04 +0800

    格式化

--- lege-management/src/views/Home.tsx
+++ lege-management/src/views/Home.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { Breadcrumb, Button, Layout, theme } from 'antd';
-import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
+import React, {useState} from 'react';
+import {Breadcrumb, Button, Layout, theme} from 'antd';
+import {Outlet, useLocation, useNavigate} from "react-router-dom";
 import MainMenu from "@/components/MainMenu";
-import { ItemType } from 'antd/es/breadcrumb/Breadcrumb';
 
-const { Header, Content, Footer, Sider } = Layout;
+const {Header, Content, Footer, Sider} = Layout;
 
 const breadNameMap: Record<string, string> = {
     "/task_management": "任务管理",
@@ -16,14 +15,14 @@ const breadNameMap: Record<string, string> = {
 const Home = () => {
     const [collapsed, setCollapsed] = useState(false);
     const {
-        token: { colorBgContainer },
+        token: {colorBgContainer},
     } = theme.useToken();
     // 面包屑
     const navigateTo = useNavigate();
     const location = useLocation();
     const pathSnippets = location.pathname.split('/').filter((i) => i)
 
-    const extraBreadcrimbItems = pathSnippets.map(
+    const extraBreadcrumbItems = pathSnippets.map(
         (_, index) => {
             const url = `/${pathSnippets.slice(0, index + 1).join('/')}`
             return {
@@ -52,35 +51,33 @@ const Home = () => {
             >Dashboard</Button>,
             key: 'home'
         }
-    ].concat(extraBreadcrimbItems);
-
-    const [breadItems, setBreadItem] = useState<ItemType[]>([]);
+    ].concat(extraBreadcrumbItems);
 
     return (
-        <Layout style={{ minHeight: '100vh' }}>
+        <Layout style={{minHeight: '100vh'}}>
             <Sider
                 collapsible
                 collapsed={collapsed}
                 onCollapse={(value) => setCollapsed(value)}
             >
-                <div style={{ height: 32, margin: 16, background: 'rgba(255, 255, 255, 0.2)' }} />
-                <MainMenu />
+                <div style={{height: 32, margin: 16, background: 'rgba(255, 255, 255, 0.2)'}}/>
+                <MainMenu/>
             </Sider>
             <Layout className="site-layout">
-                <Header style={{ padding: 0, background: colorBgContainer }}>
+                <Header style={{padding: 0, background: colorBgContainer}}>
                     <Breadcrumb
-                        style={{ lineHeight: "64px", marginLeft: "16px" }}
+                        style={{lineHeight: "64px", marginLeft: "16px"}}
                         items={breadcrumbItems}
                     />
                 </Header>
                 <Content
-                    style={{ margin: "16px 16px 0", background: colorBgContainer }}
+                    style={{margin: "16px 16px 0", background: colorBgContainer}}
                     className="seit-layout-background"
                 >
                     {/*窗口部分*/}
-                    <Outlet />
+                    <Outlet/>
                 </Content>
-                <Footer style={{ textAlign: 'center', padding: 0, lineHeight: "48px" }}>Ant Design ©2023 Created by Ant
+                <Footer style={{textAlign: 'center', padding: 0, lineHeight: "48px"}}>Ant Design ©2023 Created by Ant
                     UED</Footer>
             </Layout>
         </Layout>
